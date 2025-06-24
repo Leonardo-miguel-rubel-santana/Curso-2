331702eebe3135a172877d5077f84ba819fbc05e
foden doku <leonardo.rubel.santana@escola.pr.gov.br>
2025-06-24 07:58:49 -0300

    ll

--- script.js
+++ script.js
@@ -3,5 +3,27 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    alert('fui clicado!');
+   const texto= document.querySelector('#entrada-de-texto').valeu;
+   const campoResultado = document.querySelector('#resultado-palavrachave');
+   const palavras  = processaTexto (texto);
+
+   campoResultado.textContent = palavrasChave.join(","); 
+ }
+console.log(frequencias);
+return palavras;
 }
+function contaFrequencias(palavras){
+    
+}
+function processaTexto(texto){
+   let palavras = texto.split(/\P{L}+/l);
+   contaFrequencias(palavras);
+   let frequencias = {;
+   for(let i of  palavras){
+       frequencias [i] =0;
+       for (let j of  palavras)
+           if (i==j){
+               frequencias[i]++;
+           }
+       }
+   }
